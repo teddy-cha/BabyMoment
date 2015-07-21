eb58eec8acc79826bc452f713f6cfcbcc6fb2c3c
chayongbin <teddy418@nhnnext.org>
2015-07-21 14:47:12 +0900

    [Task] Add BaseLoginActivity, BabyMomentToast, LogUtil (Base Code)

---
+++ app/src/main/java/afterteam/com/babymoment/utils/BabyMomentToast.java
@@ -0,0 +1,29 @@
+package afterteam.com.babymoment.utils;
+
+import android.content.Context;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import afterteam.com.babymoment.R;
+
+/**
+ * Created by chayongbin on 15. 7. 21..
+ */
+public class BabyMomentToast {
+    public static Toast makeToast(Context context, String body, int duration){
+        LayoutInflater inflater;
+        inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View v = inflater.inflate(R.layout.view_toast, null);
+        TextView text = (TextView) v.findViewById(R.id.message);
+        text.setText(body);
+
+        Toast toast = new Toast(context);
+        toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+        toast.setView(v);
+        toast.setDuration(duration);
+        return toast;
+    }
+}
