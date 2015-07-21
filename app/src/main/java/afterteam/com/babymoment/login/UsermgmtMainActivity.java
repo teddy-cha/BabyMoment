92a8a16dfd688e353165ed678f29d713d3691262
chayongbin <teddy418@nhnnext.org>
2015-07-21 18:10:59 +0900

    [Task] Add User Profile Layout

--- app/src/main/java/afterteam/com/babymoment/login/UsermgmtMainActivity.java
+++ app/src/main/java/afterteam/com/babymoment/login/UsermgmtMainActivity.java
@@ -1,6 +1,12 @@
 package afterteam.com.babymoment.login;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.kakao.usermgmt.UserProfile;
+
+import afterteam.com.babymoment.Base.ProfileLayout;
 
 /**
  * The main page is the page that users see subscription.
@@ -9,4 +15,49 @@ import android.app.Activity;
  * @Reference Kakao Sample App
  */
 public class UsermgmtMainActivity extends Activity {
+    
+    private UserProfile userProfile;
+    private ProfileLayout profileLayout;
+
+
+    /**
+     * If User information passed by the sign is present, save
+     * @param savedInstanceState Existing objects are stored session information.
+     */
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        initializeView();
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        userProfile = UserProfile.loadFromCache();
+        if(userProfile != null) {
+            showProfile();
+        }
+    }
+
+    private void redirectLoginActivity() {
+        Intent intent = new Intent(this, UserMgmtLoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    private void redirectSignupActivity() {
+        Intent intent = new Intent(this, UsermgmtSignupActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    private void initializeView() {
+    }
+
+    private void showProfile() {
+        if (profileLayout != null) {
+            profileLayout.setUserProfile(userProfile);
+        }
+    }
+
 }
