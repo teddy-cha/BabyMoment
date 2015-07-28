44b310140af47e8df54ef0051e4e78e6ce3dd0d7
chayongbin <teddy418@nhnnext.org>
2015-07-28 10:54:33 +0900

    [Refactoring] rename package

--- app/src/main/java/afterteam/com/babymoment/Base/BaseLoginActivity.java
+++ app/src/main/java/afterteam/com/babymoment/Base/BaseLoginActivity.java
@@ -27,7 +27,7 @@ public class BaseLoginActivity extends Activity {
 
     private final String TAG = LogUtils.makeTag(this.getClass().getSimpleName());
 
-    private afterteam.com.babymoment.base.BabyMomentLoginButton loginButton;
+    private BabyMomentLoginButton loginButton;
     private final SessionCallback mySessionCallback = new MySessionStatusCallback();
     private Session session;
 
@@ -44,7 +44,7 @@ public class BaseLoginActivity extends Activity {
         setContentView(R.layout.layout_base_kakao_login);
 
         // Find the login button.
-        loginButton = (afterteam.com.babymoment.base.BabyMomentLoginButton) findViewById(R.id.kakao_login);
+        loginButton = (BabyMomentLoginButton) findViewById(R.id.kakao_login);
 
         session = Session.getCurrentSession();
         session.addCallback(mySessionCallback);
