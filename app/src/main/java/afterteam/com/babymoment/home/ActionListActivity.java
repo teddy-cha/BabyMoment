1465f0fb87f676f984a3a0b653313c8757f29a40
luvgaram <garam@nhnnext.org>
2015-07-30 21:23:37 +0900

    [Task] home main-bottom button

--- app/src/main/java/afterteam/com/babymoment/home/ActionListActivity.java
+++ app/src/main/java/afterteam/com/babymoment/home/ActionListActivity.java
@@ -4,18 +4,23 @@ import java.util.ArrayList;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.ExpandableListView;
 import android.widget.ExpandableListView.OnChildClickListener;
 import android.widget.ExpandableListView.OnGroupClickListener;
 import android.widget.ExpandableListView.OnGroupCollapseListener;
 import android.widget.ExpandableListView.OnGroupExpandListener;
-import android.widget.Toast;
 
 import afterteam.com.babymoment.R;
 
 
-public class ActionListActivity extends Activity {
+public class ActionListActivity extends ActionBarActivity {
+
+    private ArrayList<String> mGroupList;
+    private ArrayList<ArrayList<ActionDTO>> mChildList;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -24,50 +29,8 @@ public class ActionListActivity extends Activity {
 
         setLayout();
 
-        ArrayList<String> mGroupList = new ArrayList<>();
-        ArrayList<ArrayList<ActionDTO>> mChildList = new ArrayList<>();
-        ArrayList<ActionDTO> mChildListContent1 = new ArrayList<>();
-        ArrayList<ActionDTO> mChildListContent2 = new ArrayList<>();
-        ArrayList<ActionDTO> mChildListContent3 = new ArrayList<>();
-
-
-        mGroupList.add("2015-07-25");
-        mGroupList.add("2015-07-24");
-        mGroupList.add("2015-07-23");
-
-        ActionDTO test11 = new ActionDTO(1, 2, 1, "AM 06:30", "~ AM 09:00", "3");
-        ActionDTO test12 = new ActionDTO(2, 4, 1, "AM 09:00", "250ml", "2");
-        ActionDTO test13 = new ActionDTO(3, 3, 1, "AM 09:05", "poo, picture", "1");
-        ActionDTO test14 = new ActionDTO(4, 4, 2, "AM 09:09", "왼쪽, 10분", "1");
-        ActionDTO test15 = new ActionDTO(5, 2, 2, "AM 09:10", "~ AM 10:00", "1");
-        ActionDTO test16 = new ActionDTO(6, 1, 2, "AM 10:00", "콧물", "3");
-        ActionDTO test17 = new ActionDTO(7, 3, 2, "AM 10:05", "pee", "2");
-        ActionDTO test18 = new ActionDTO(8, 3, 3, "AM 10:10", "", "1");
-
-        mChildListContent1.add(test11);
-        mChildListContent1.add(test12);
-        mChildListContent1.add(test13);
-        mChildListContent1.add(test14);
-        mChildListContent1.add(test15);
-        mChildListContent1.add(test16);
-        mChildListContent1.add(test17);
-        mChildListContent1.add(test18);
-
-        mChildListContent2.add(test11);
-        mChildListContent2.add(test12);
-        mChildListContent2.add(test13);
-        mChildListContent2.add(test14);
-        mChildListContent2.add(test15);
-        mChildListContent2.add(test16);
-
-        mChildListContent3.add(test11);
-        mChildListContent3.add(test12);
-        mChildListContent3.add(test13);
-        mChildListContent3.add(test14);
-
-        mChildList.add(mChildListContent1);
-        mChildList.add(mChildListContent2);
-        mChildList.add(mChildListContent3);
+        // temporarily input dummy data by hard coding
+        setDummyData();
 
         final BaseExpandableAdapter mBaseExpandableAdapter = new BaseExpandableAdapter(this, mGroupList, mChildList);
         mListView.setAdapter(mBaseExpandableAdapter);
@@ -113,16 +76,68 @@ public class ActionListActivity extends Activity {
         });
 
         // 맨 위 그룹을 펼침
-//        int groupCount = mBaseExpandableAdapter.getGroupCount();
         mListView.expandGroup(0);
     }
 
-    /*
-     * Layout
-     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_action_list, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    // Layout
+
     private ExpandableListView mListView;
 
     private void setLayout() {
         mListView = (ExpandableListView) findViewById(R.id.elv_list);
+    }
+
+    private void setDummyData() {
+        mGroupList = new ArrayList<>();
+        mChildList = new ArrayList<>();
+        ArrayList<ActionDTO> mChildListContent1 = new ArrayList<>();
+        ArrayList<ActionDTO> mChildListContent2 = new ArrayList<>();
+        ArrayList<ActionDTO> mChildListContent3 = new ArrayList<>();
+
+
+        mGroupList.add("2015-07-25");
+        mGroupList.add("2015-07-24");
+        mGroupList.add("2015-07-23");
+
+        ActionDTO test11 = new ActionDTO(1, 2, 1, "AM 06:30", "~ AM 09:00", "3");
+        ActionDTO test12 = new ActionDTO(2, 4, 1, "AM 09:00", "250ml", "2");
+        ActionDTO test13 = new ActionDTO(3, 3, 1, "AM 09:05", "poo, picture", "1");
+        ActionDTO test14 = new ActionDTO(4, 4, 2, "AM 09:09", "왼쪽, 10분", "1");
+        ActionDTO test15 = new ActionDTO(5, 2, 2, "AM 09:10", "~ AM 10:00", "1");
+        ActionDTO test16 = new ActionDTO(6, 1, 2, "AM 10:00", "콧물", "3");
+        ActionDTO test17 = new ActionDTO(7, 3, 2, "AM 10:05", "pee", "2");
+        ActionDTO test18 = new ActionDTO(8, 3, 3, "AM 10:10", "", "1");
+
+        mChildListContent1.add(test11);
+        mChildListContent1.add(test12);
+        mChildListContent1.add(test13);
+        mChildListContent1.add(test14);
+        mChildListContent1.add(test15);
+        mChildListContent1.add(test16);
+        mChildListContent1.add(test17);
+        mChildListContent1.add(test18);
+
+        mChildListContent2.add(test11);
+        mChildListContent2.add(test12);
+        mChildListContent2.add(test13);
+        mChildListContent2.add(test14);
+        mChildListContent2.add(test15);
+        mChildListContent2.add(test16);
+
+        mChildListContent3.add(test11);
+        mChildListContent3.add(test12);
+        mChildListContent3.add(test13);
+        mChildListContent3.add(test14);
+
+        mChildList.add(mChildListContent1);
+        mChildList.add(mChildListContent2);
+        mChildList.add(mChildListContent3);
     }
 }
