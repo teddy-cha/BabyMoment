7b258c0691e2f56e4c28b01de9a24c74cad021d6
chayongbin <teddy418@nhnnext.org>
2015-08-04 04:11:46 +0900

    [Task] Add Action Model

---
+++ app/src/main/java/afterteam/com/babymoment/model/Action.java
@@ -0,0 +1,78 @@
+package afterteam.com.babymoment.model;
+
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+
+/**
+ * Created by chayongbin on 15. 8. 4..
+ */
+public class Action extends RealmObject{
+
+    @PrimaryKey
+    private int         action_id;
+
+    private int         type;
+    private int         count;
+    private String      time;
+    private String      detail;
+    private String      photo;
+    // 1:1 관계 테스트
+    private Baby        baby;
+
+
+    public int getAction_id() {
+        return action_id;
+    }
+
+    public void setAction_id(int action_id) {
+        this.action_id = action_id;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public Baby getBaby() {
+        return baby;
+    }
+
+    public void setBaby(Baby baby) {
+        this.baby = baby;
+    }
+}
